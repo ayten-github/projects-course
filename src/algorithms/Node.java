de4054f3830690e77aadc77e4b49cf4c8b51d141
Ayten <ababakishiyeva@std.beu.edu.az>
2024-10-04 06:22:52 +0300

    adding studentApp

---
+++ src/algorithms/Node.java
@@ -0,0 +1,11 @@
+package algorithms;
+
+public class Node<T> {
+    T value;
+    algorithms.Node<T> next;
+
+    public Node(T value) {
+        this.value = value;
+        this.next = null;
+    }
+}
